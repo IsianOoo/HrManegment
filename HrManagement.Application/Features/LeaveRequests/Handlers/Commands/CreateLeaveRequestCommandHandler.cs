41f9e7d8d270aaf1de83ccb9303a4918427e8e8e
Krystian <Krystian@DESKTOP-DA1HJHA>
2023-07-10 13:40:38 +0200

    Add new commands

---
+++ HrManagement.Application/Features/LeaveRequests/Handlers/Commands/CreateLeaveRequestCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HrManagement.Application.Features.LeaveRequests.Handlers.Commands
+{
+    internal class CreateLeaveAllocationCommandHandler
+    {
+    }
+}
