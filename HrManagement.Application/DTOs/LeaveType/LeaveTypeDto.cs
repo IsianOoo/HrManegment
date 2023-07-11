624d095d72c117ac5c54fcb68cb7d660b3b0d25e
Krystian <Krystian@DESKTOP-DA1HJHA>
2023-07-11 11:36:34 +0200

    Validation

--- HrManagement.Application/DTOs/LeaveType/LeaveTypeDto.cs
+++ HrManagement.Application/DTOs/LeaveType/LeaveTypeDto.cs
@@ -10,6 +10,6 @@ namespace HrManagement.Application.DTOs.LeaveType
     public class LeaveTypeDto : BaseDto, ILeaveTypeDto
     {
         public string Name { get; set; }
-        public int DefultDays { get; set; }
+        public int DefaultDays { get; set; }
     }
 }
